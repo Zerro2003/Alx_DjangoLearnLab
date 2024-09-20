f7b11c0b76d7b0aab1000214fae8c345dd57463d
Elie Mugisha <mugishaelie160@gmail.com>
2024-09-20 20:32:56 +0200

    the correct

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'the_x_and_o',
+        'USER': 'root',
+        'PASSWORD': '111elie3352024Lx',
+        'HOST': 'localhost',  # If not localhost
+        'PORT': '3310',          # Default MySQL port
+        
     }
 }
 
