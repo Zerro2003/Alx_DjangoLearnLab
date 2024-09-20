9827648075ed651806111180d9befe2dd11f63c1
Elie Mugisha <mugishaelie160@gmail.com>
2024-09-20 19:32:47 +0200

    the commited folder are okay

---
+++ api_project/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
