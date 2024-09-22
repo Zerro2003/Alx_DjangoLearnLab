3510764c2ddf1c350a5ea056d468fe7f6bb9ee6c
Elie Mugisha <mugishaelie160@gmail.com>
2024-09-22 15:13:38 +0200

    aha

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post
+from .models import Comment
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -12,3 +13,15 @@ class PostForm(forms.ModelForm):
     class meta:
         model = Post
         fields = ['title','content']
+
+class CommentForm(forms.ModelForm):
+    class meta:
+        model = Comment
+        fields = ['content']
+        widjets = {
+            'content': forms.Textarea(attrs={'class': 'form-control','rows': 3}),
+
+        }
+        labels = {
+            'content': 'Add your comment here:',
+        }
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from django.shortcuts import get_object_or_404
+from . forms import CommentForm
+from tokenize import Comment
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -14,7 +17,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-published_date']
 
 # DetailView to display a single post
 class PostDetailView(DetailView):
@@ -67,6 +70,43 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user != comment.author:
+        return redirect('post_detail', post_id=comment.post.id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id)
+    if request.user == comment.author:
+        comment.delete()
+    return redirect('post_detail', post_id=comment.post.id)
 
 @login_required
 def profile(request):
