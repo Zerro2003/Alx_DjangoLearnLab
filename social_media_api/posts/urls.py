863bcfaa32ac2dad50e43dc2686cd02212d3f5b7
Elie Mugisha <mugishaelie160@gmail.com>
2024-09-25 09:17:14 +0200

    shd

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
-
+from django.contrib import admin
 router = DefaultRouter()
 router.register('posts', views.PostViewSet)
 router.register('comments', views.CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('posts.urls')),
 ]
